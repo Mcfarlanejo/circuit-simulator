e388f8359a7fb9ccb63a2ffdf58e97a4a75bd6cc
monty1289 <64822697+monty1289@users.noreply.github.com>
2023-03-30 11:10:37 +1300

    script added for voltage to be shown on meter canvas, still needs work

---
+++ Circuit Simulator/Assets/Scripts/CurrentOutput.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentOutput : MonoBehaviour
+{
+    public AnchorPoint negitive;
+    public AnchorPoint positive;
+
+    public Canvas canvas;
+    
+    //show voltage output of connected object 
+}
