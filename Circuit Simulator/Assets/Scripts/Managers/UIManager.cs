86212d272c1a0e7adbc4b1f8e59ca79fa6586da5
bchorne <ben.chris.horne@gmail.com>
2023-05-11 12:48:44 +1200

    Toggled tab sprite on tab press.

--- Circuit Simulator/Assets/Scripts/Managers/UIManager.cs
+++ Circuit Simulator/Assets/Scripts/Managers/UIManager.cs
@@ -2,8 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject flip;
+    private Image tab;
+    public Sprite unpressed;
+    public Sprite pressed;
+    private bool front;
 
+    void Start()
+    {
+        tab = flip.transform.GetChild(0).gameObject.GetComponent<Image>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("tab"))
+        {
+            if (!front)
+            {
+                tab.sprite = pressed;
+            }
+            else
+            {
+                tab.sprite = unpressed;
+            }
+            front = !front;
+        }
+    }
 }
