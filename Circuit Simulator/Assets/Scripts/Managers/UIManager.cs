ff55a64fe2e9f04857597149cfab85abc8fe633e
bchorne <ben.chris.horne@gmail.com>
2023-05-11 13:00:16 +1200

    Made arrow sprite rotate on tab press.

--- Circuit Simulator/Assets/Scripts/Managers/UIManager.cs
+++ Circuit Simulator/Assets/Scripts/Managers/UIManager.cs
@@ -6,30 +6,47 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject flip;
-    private Image tab;
+    public GameObject tab;
+    private GameObject flip;
     public Sprite unpressed;
     public Sprite pressed;
     private bool front;
+    
+    private Quaternion lerpRot; //Lerp rotation target
+    private float curLerpTime; //This and next are for smoothing time values.
+    private float lerpTime = 1f;
 
     void Start()
     {
-        tab = flip.transform.GetChild(0).gameObject.GetComponent<Image>();
+        flip = tab.transform.GetChild(0).gameObject;
+        lerpRot = Quaternion.Euler(0, 0, 0);
     }
     
     void Update()
     {
+        curLerpTime += Time.deltaTime;
+        if (curLerpTime > lerpTime)
+        {
+            curLerpTime = lerpTime;
+        }
+        float perc = curLerpTime / lerpTime;
+
+        flip.transform.rotation = Quaternion.Lerp(flip.transform.rotation, lerpRot, perc);
+
         if (Input.GetKeyDown("tab"))
         {
             if (!front)
             {
-                tab.sprite = pressed;
+                tab.GetComponent<Image>().sprite = pressed;
+                lerpRot = Quaternion.Euler(0, 0, 180);
             }
             else
             {
-                tab.sprite = unpressed;
+                tab.GetComponent<Image>().sprite = unpressed;
+                lerpRot = Quaternion.Euler(0, 0, 0);
             }
             front = !front;
+            curLerpTime = 0f;
         }
     }
 }
